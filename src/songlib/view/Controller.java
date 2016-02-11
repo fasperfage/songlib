b5a468afed9d022719aab899a2c1cdfc12dcd874
calvillain <hobbes102@gmail.com>
2016-02-11 00:06:07 -0500

    basic layout, csv input file

--- src/songlib/view/Controller.java
+++ src/songlib/view/Controller.java
@@ -1,10 +1,18 @@
 package songlib.view;
 
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class Controller {
 
@@ -15,19 +23,47 @@ public class Controller {
     private URL location;
 
     @FXML
-    private Button btn;
+    private Button addBtn;
+    
+    @FXML
+    private Button delBtn;
+    
+    @FXML
+    private Button editBtn;
+    
+    @FXML
+    private TextArea songList;
+    
+    @FXML
+    private Label statusBar;
 
     @FXML
     public void onButton(ActionEvent e) {
-    	if (e.getSource() == btn){
-    		btn.setText("Hello!");
+    	if (e.getSource() == addBtn){
+    		statusBar.setText("added!");
     		return;
     	}
     }
 
     @FXML
     void initialize() {
-        assert btn != null : "fx:id=\"btn\" was not injected: check your FXML file 'Layout.fxml'.";
+    	
+    	try {
+            Scanner s = new Scanner(new File("songs.csv")).useDelimiter("\"(,\")?");
+            while (s.hasNext()) {
+            	
+                songList.appendText(s.next() + "\n"); // adds each song in list to textarea
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);	//no file
+        }
+    	
+    	assert addBtn != null : "fx:id=\"addBtn\" was not injected: check your FXML file 'Layout.fxml'.";
+        assert songList != null : "fx:id=\"songList\" was not injected: check your FXML file 'Layout.fxml'.";
+        assert delBtn != null : "fx:id=\"delBtn\" was not injected: check your FXML file 'Layout.fxml'.";
+        assert editBtn != null : "fx:id=\"editBtn\" was not injected: check your FXML file 'Layout.fxml'.";
+        assert statusBar != null : "fx:id=\"statusBar\" was not injected: check your FXML file 'Layout.fxml'.";
+
 
     }
 }
